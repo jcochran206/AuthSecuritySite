8b3da407f7d0fb7ed04218c649caf87cbd535bd2
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-03-11 21:19:56 -0700

    level one: got user and password to check against database

--- Secrets/app.js
+++ Secrets/app.js
@@ -5,27 +5,63 @@ const mongoose = require("mongoose");
 const ejs = require("ejs");
 
 const app = express();
-const port = 3005;
+const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 //model connection
-//mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 //model schema
+const userSchema = {
+  email: String,
+  password: String
+};
+// user model
+const User = new mongoose.model("User", userSchema);
 
 //////////////////// routes ////////////////////
 app.get('/', function(req, res){
   res.render("home");
 });
 
-app.get('/register', function(req, res){
+app.route("/register")
+.get( function(req, res){
   res.render("register");
+})
+.post(function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
 });
 
-app.get('/login', function(req, res){
+app.route("/login")
+.get(function(req, res){
   res.render("login");
+})
+.post(function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render('secrets');
+        }
+      }
+    }
+  });
 });
 //////////// server listen to /////////////////
 app.listen(port, function(){
