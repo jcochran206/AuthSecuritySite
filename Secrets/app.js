61177f9499771712ac8fe3b67d057a7eba9ed072
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-03-12 20:37:58 -0700

    level 2 - encryption of database

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,8 +1,11 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 const port = 3000;
@@ -14,10 +17,14 @@ app.use(express.static('public'));
 //model connection
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 //model schema
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']  });
+
 // user model
 const User = new mongoose.model("User", userSchema);
 
