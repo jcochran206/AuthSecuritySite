6d318d0b3ee47f74c4718b3e17be9034ba773376
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-03-13 20:38:32 -0700

    level 3 - removed encryption mongoose and replaced with MD5 package to hash password

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
 require('dotenv').config();
+const md5 = require('md5')
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
+
 
 
 const app = express();
@@ -23,7 +24,6 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']  });
 
 // user model
 const User = new mongoose.model("User", userSchema);
@@ -40,7 +40,7 @@ app.route("/register")
 .post(function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -57,7 +57,7 @@ app.route("/login")
 })
 .post(function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
